7c9574c0185be154ce596b543453491e9e0dd2ea
Randy D. Wallace Jr <randy@randywallace.com>
2013-09-18 02:53:52 -0400

    Add Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Tugboat::Application.routes.draw do
+  get "container/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
