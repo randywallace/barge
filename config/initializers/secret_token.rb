8e64a436cb7537442fb47ad532addc4818dafb88
Randy D. Wallace Jr <randy@randywallace.com>
2013-09-18 01:56:47 -0400

    Actually add rails 4.0.0 files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tugboat::Application.config.secret_key_base = '239414341c0fd7e530be1f176f0c66c5040b899c2fd6c2ff2919e5a0dcea4ff73545f646d4dbd83270489046cf9d0ac7460a83ae9777426e1544527725f8fca6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tugboat::Application.config.session_store :cookie_store, key: '_tugboat_session'
