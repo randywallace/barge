afc16216ad0b6a3d58ac3b2c4643319f43264103
Randy D. Wallace Jr <randy@randywallace.com>
2013-09-18 02:41:23 -0400

    Move Coveralls.io code to very top of test spec

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,10 @@
-ENV["RAILS_ENV"] ||= "test"
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
-
 # For Code Coverage at coveralls.io
 require 'coveralls'
 Coveralls.wear!('rails')
+
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
 
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
